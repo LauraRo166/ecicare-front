647e5e42bb4c04be1d5f159b641894d5a103f1ca
LauraRo166 <laura.rsanchez@mail.escuelaing.edu.co>
2025-08-25 23:23:35 -0500

    feat(modules): Page for modules

--- src/pages/ModulesPage.tsx
+++ src/pages/ModulesPage.tsx
@@ -1,9 +1,37 @@
-import { NotAvailable } from "@/components/NotAvailable";
+import { useState } from "react";
+import { DashboardModules } from "@/components/dashboards/DashboardModules.tsx";
+import { SectionActions } from "@/components/SectionActions.tsx";
+import { DashboardHeader } from "@/components/common/DashboardHeader.tsx";
+import { useAuth } from "@/contexts/AuthContext.tsx";
+import { AddModuleModal } from "@/components/modals/AddModuleModal.tsx";
 
 export const ModulesPage = () => {
+    const { logout } = useAuth();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
-        <div className="flex md:flex-row h-screen w-full items-center justify-center">
-            <NotAvailable />
+        <div className="flex flex-col h-screen w-full">
+            <div className="min-h-screen w-full bg-[#fceceb] p-0">
+                {/* Header */}
+                <DashboardHeader title="Administrador" onLogout={logout} />
+
+                {/* Acciones */}
+                <div className="flex gap-4 px-8 py-4 justify-center">
+                    <SectionActions
+                        title="Módulos"
+                        onAdd={() => setIsModalOpen(true)} // abrir modal
+                    />
+                </div>
+
+                {/* Módulos */}
+                <DashboardModules />
+
+                {/* Modal */}
+                <AddModuleModal
+                    isOpen={isModalOpen}
+                    onClose={() => setIsModalOpen(false)}
+                />
+            </div>
         </div>
     );
 };
